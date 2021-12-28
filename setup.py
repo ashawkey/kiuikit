a8f14179ce555e9a3bf795f7ebd322091e121662
hawkey <ashawkey1999@gmail.com>
2021-12-28 16:03:07 +0800

    auto publish

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 if __name__ == '__main__':
     setup(
         name="kiui",
-        version='0.1.0',
+        version='0.1.1',
         description="self-use toolkits",
         long_description=open('README.md', encoding='utf-8').read(),
         long_description_content_type='text/markdown',
