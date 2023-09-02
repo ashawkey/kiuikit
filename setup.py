8ea6bc7fb649da260e88c86c5b3dfab3dcb15022
ashawkey <ashawkey1999@gmail.com>
2023-09-02 22:20:30 +0800

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 if __name__ == "__main__":
     setup(
         name="kiui",
-        version="0.1.7",
+        version="0.1.8",
         description="self-use toolkits",
         long_description=open("README.md", encoding="utf-8").read(),
         long_description_content_type="text/markdown",
