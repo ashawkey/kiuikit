3314e88a3934bae0a9363bb2bbb38fed3e125cc8
ashawkey <ashawkey1999@gmail.com>
2023-09-03 11:19:26 +0800

    fix cli import

--- kiui/__init__.py
+++ kiui/__init__.py
@@ -7,7 +7,7 @@ import lazy_loader
 
 __getattr__, __dir__, _ = lazy_loader.attach(
     __name__,
-    submodules=["op", "vis", "cam", "mesh", "cli", "sr", "render"],
+    submodules=["op", "vis", "cam", "mesh", "sr", "render", "cli", "lpips"],
     submod_attrs={
         "utils": ["lo", "read_json", "write_json", "read_image", "write_image"],
         "env": ["env"],
