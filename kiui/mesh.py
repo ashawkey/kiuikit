fix mesh rotation

kiui/mesh.py
@@ -78,7 +78,7 @@ class Mesh:
             if '1' in front_dir:
                 T @= torch.tensor([[0, -1, 0], [1, 0, 0], [0, 0, 1]], device=mesh.device, dtype=torch.float32) 
             elif '2' in front_dir:
-                T @= torch.tensor([[-1, 0, 0], [0, 1, 0], [0, 0, 1]], device=mesh.device, dtype=torch.float32) 
+                T @= torch.tensor([[1, 0, 0], [0, -1, 0], [0, 0, 1]], device=mesh.device, dtype=torch.float32) 
             elif '3' in front_dir:
                 T @= torch.tensor([[0, 1, 0], [-1, 0, 0], [0, 0, 1]], device=mesh.device, dtype=torch.float32) 
             mesh.v @= T